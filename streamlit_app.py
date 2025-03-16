199f13174451f5940f7e20b290f21da622fc4c79
ValtsuL1 <linnavaltteri@gmail.com>
2025-03-16 18:16:27 +0200

    added sentiment analysis, chart and score buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,45 @@
 import streamlit as st
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+nltk.download('all')
+
+analyzer = SentimentIntensityAnalyzer()
+
+st.title("Sentiment analysis")
+
+sentence = st.text_input(
+    "Write sentence here"
 )
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
+st.button("Submit", on_click=click_button)
+
+if st.session_state.clicked:
+    scores = analyzer.polarity_scores(sentence)
+    
+    sentiment = "Neutral"
+
+    if scores['compound'] > 0.5:
+        sentiment = "Positive"
+    elif scores['compound'] < -0.5:
+        sentiment = "Negative"
+
+    "Sentiment: " + sentiment
+
+    if st.checkbox("Show scores"):
+        "Negative: " + str(scores['neg'])
+        "Neutral: " + str(scores['neu'])
+        "Positive: " + str(scores['pos'])
+        "Compound: " + str(scores['compound'])
+
+    if st.checkbox("Show bar chart"):
+        chart_data = pd.Series(scores)
+
+        st.bar_chart(chart_data)
